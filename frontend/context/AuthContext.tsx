d31be3d9f810d737bce1e18ffaf63e9f4358c4e5
Dinie Zikry <77114684+diniezikryy@users.noreply.github.com>
2022-09-18 18:19:35 +0800

    protected route (not HOC)

---
+++ frontend/context/AuthContext.tsx
@@ -0,0 +1,43 @@
+import { createContext, useState, useEffect } from "react";
+import jwt_decode from "jwt-decode";
+
+const AuthContext = createContext("Default Value");
+
+export default AuthContext;
+
+export const AuthProvider = ({ children }: any) => {
+  const [authTokens, setAuthTokens] = useState(null);
+  const [user, setUser] = useState(null);
+
+  let loginUser = async (e: any) => {
+    e.preventDefault();
+
+    let response = await fetch("http://127.0.0.1:8000/api/token/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username: e.target.username.value,
+        password: e.target.password.value,
+      }),
+    });
+
+    const data = await response.json();
+
+    if (response.status === 200) {
+      setAuthTokens(data);
+      setUser(jwt_decode(data.access));
+    } else {
+      alert("Something went wrong!");
+    }
+  };
+
+  const contextData = {
+    loginUser: loginUser,
+    user: user,
+  };
+  return (
+    <AuthContext.Provider value={contextData}>{children}</AuthContext.Provider>
+  );
+};
--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,8 +1,17 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Header from "../components/Header";
+import { AuthProvider } from "../context/AuthContext";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <AuthProvider>
+        <Header />
+        <Component {...pageProps} />
+      </AuthProvider>
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
