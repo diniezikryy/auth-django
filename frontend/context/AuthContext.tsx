7eb92cb5298369283b7fbf316bb33b10b075ec1b
Dinie Zikry <77114684+diniezikryy@users.noreply.github.com>
2022-09-19 23:04:08 +0800

    before implementing cookie-token auth

--- frontend/context/AuthContext.tsx
+++ frontend/context/AuthContext.tsx
@@ -1,5 +1,6 @@
 import { createContext, useState, useEffect } from "react";
 import jwt_decode from "jwt-decode";
+import { useRouter } from "next/router";
 
 const AuthContext = createContext("Default Value");
 
@@ -9,10 +10,20 @@ export const AuthProvider = ({ children }: any) => {
   const [authTokens, setAuthTokens] = useState(null);
   const [user, setUser] = useState(null);
 
-  let loginUser = async (e: any) => {
+  useEffect(() => {
+    setAuthTokens(
+      localStorage.getItem("authTokens")
+        ? JSON.parse(localStorage.getItem("authTokens"))
+        : null
+    );
+  }, []);
+
+  const router = useRouter();
+
+  const loginUser = async (e: any) => {
     e.preventDefault();
 
-    let response = await fetch("http://127.0.0.1:8000/api/token/", {
+    const response = await fetch("http://127.0.0.1:8000/api/token/", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -28,6 +39,8 @@ export const AuthProvider = ({ children }: any) => {
     if (response.status === 200) {
       setAuthTokens(data);
       setUser(jwt_decode(data.access));
+      localStorage.setItem("authTokens", JSON.stringify(data));
+      router.push("/");
     } else {
       alert("Something went wrong!");
     }
